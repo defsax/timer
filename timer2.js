a790ded2dd2d0dc8901d7c588a870306d4a2070f
Perry Defayette <defayetteperry@gmail.com>
2021-01-13 03:01:10 +0000

    Removed comments.

--- timer2.js
+++ timer2.js
@@ -23,31 +23,3 @@ stdin.on('data', (key) => {
   }
 
 });
-
-// const timer = function(timers) {
-//   if (!timers.length) {
-//     console.log('No timers selected.');
-//     return false;
-//   }
-
-//   for (let timer of timers) {
-//     if (Math.sign(timer) === -1 || isNaN(timer)) {
-//       continue;
-//     }
-//     setTimeout(() => {
-//       console.log('Beep at', timer, 'seconds.');
-//     }, timer * 1000);
-//   }
-// };
-
-// timer(process.argv.slice(2));
-
-/*
-  The user can press b and it should beep right away
-  
-  The user can input any number from 1 to 9 and it should
-      immediately output "setting timer for x seconds...", and
-      beep after that number of seconds has passed
-  
-  The user can use ctrl + c to exit the program, at which point the program should say "Thanks for using me, ciao!" before terminating
-*/
